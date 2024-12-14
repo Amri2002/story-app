410ba8f6bf2a2721b5038f3b5cb7cb094b23081f
Muhammed Amri <muhammedamri2002@gmail.com>
2024-12-15 00:59:53 +0530

    Create new user resources

--- app/Enum/PermissionsEnum.php
+++ app/Enum/PermissionsEnum.php
@@ -4,9 +4,9 @@ namespace App\Enum;
 
 enum PermissionsEnum: string
 {
-    case ManageStories = 'manage-stories';
-    case ManageUsers = 'manage-users';
-    case ManageComments = 'manage-comments';
+    case ManageStories = 'manage_stories';
+    case ManageUsers = 'manage_users';
+    case ManageComments = 'manage_comments';
     case UpvoteDownvote = 'upvote_downvote';
 
 
